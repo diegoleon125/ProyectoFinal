ac5c8196fa98382aa5cb9f9dc1e00f7711608383
Deyvi Condori <deivcondor@gmail.com>
2023-11-30 22:48:26 -0500

    Correccion grupal en Pantallas

--- Pantallas.cs
+++ Pantallas.cs
@@ -17,10 +17,10 @@ namespace ProyectoFinal
         public static int alm_contador = 1;
         public static int[] pdto_contador = new int[100];
 
-
-        //Menus
+        //Menú Principal
         public static int MenuPrincipal()
         {
+            almacen[0] = "Tiendita";
             string texto = "==================================================\n" +
                 "||\t\t\t\t\t\t||\n" +
                 "||\tSistema de Inventario \"Mi Tiendita\"\t||\n" +
@@ -31,9 +31,10 @@ namespace ProyectoFinal
                 "|| 3. Agregar y Extraer Productos\t\t||\n" +
                 "==================================================\n";
             Console.Write(texto);
-            int opcion = Operaciones.GetOpcion("Seleccione una opción y presione Enter:", texto,3);
+            int opcion = Operaciones.GetOpcion("Seleccione una opción y presione Enter: ", texto,3);
             return opcion;
         }
+        //Menú Gestionar Productos
         public static int MenuGestionarProductos()
         {
             string texto = "--------------------------------------------------\n" +
@@ -50,6 +51,7 @@ namespace ProyectoFinal
             if (opcion == 5) return 0;
             return opcion + 3;
         }
+        //Menú Gestionar Almacenes
         public static int MenuGestionarAlmacenes()
         {
             string texto = "--------------------------------------------------\n" +
@@ -63,9 +65,10 @@ namespace ProyectoFinal
             Console.Write(texto);
             int opcion = Operaciones.GetOpcion("Seleccione una opcion: ", texto, 4);
             if (opcion == 4) return 0;
-            
+
             return opcion + 7;
         }
+        //Menú Agregar y Extraer Productos
         public static int MenuAgregarYExtraerProductos()
         {
             string texto = "--------------------------------------------------\n" +
@@ -77,13 +80,13 @@ namespace ProyectoFinal
                 "|| 4. Volver al Menú Principal                  ||\n" +
                 "--------------------------------------------------\n";
             Console.Write(texto);
-            int opcion = Operaciones.GetOpcion("Seleccione una opcion: ", texto, 4);
+            int opcion = Operaciones.GetOpcion("Seleccione una opción: ", texto, 4);
             if (opcion == 4) return 0;
             return opcion + 10;
                
         }
 
-        //Menu-Gestionar Productos
+        //Pantalla para Agregar Producto 
         public static int AgregarProducto()
         {
             string texto = "===== Pantalla para Agregar Producto =====\n" +
@@ -96,7 +99,7 @@ namespace ProyectoFinal
             pdto_contador[0]++;
             return 1;
         }
-        //Menu-Eliminar Productos
+        //Pantalla para Eliminar Producto 
         public static int EliminarProducto()
         {
             string texto = "===== Pantalla para Eliminar Producto =====\n" +
@@ -120,8 +123,8 @@ namespace ProyectoFinal
                     precio[0,i] = precio[0,i+1];
                     cantidad[0, i] = cantidad[0, i + 1];
                 }
-            pdto_contador[0]--;
-            Console.WriteLine("Producto eliminado exitosamente.");
+                pdto_contador[0]--;
+                Console.WriteLine("Producto eliminado exitosamente.");
             } 
             else
             {
@@ -130,32 +133,32 @@ namespace ProyectoFinal
             Console.ReadKey();
                 return 1;
         }
-
+        //Pantalla para Modificar Producto 
         public static int ModificarProducto()
         {
-            string texto = "==== Pantalla para Modificar Prodcto ==== \n" +
-                           "--------------------------------------------------\n" +
-                           "Ingrese el nombre del producto a modificar: ";
+            string texto = "==== Pantalla para Modificar Producto ==== \n" +
+                           "--------------------------------------------------\n";
+
             Console.Write(texto);
-            string modificar = Console.ReadLine();
-            int posicion = 0;
+            string modificar = Operaciones.GetString("Ingresar el nombre del producto a modificar: ");
+            int posicion = -1;
             for (int i = 0; i < pdto_contador[0]; i++)
             {
                 if (producto[0, i] == modificar) posicion = i;
             }
-            string texto1 = "Ingrese el nuevo precio: ";
-            Console.Write(texto1);
-            precio[0, posicion] = float.Parse(Console.ReadLine());
-            string texto2 = "Ingrese la nueva cantidad: ";
-            Console.Write(texto2);
-            cantidad[0, posicion] = int.Parse(Console.ReadLine());
-            string texto3 = "--------------------------------------------------\n" +
-                            "Producto modificado exitosamente.";
-            Console.Write(texto3);
-            Console.ReadKey();
+                if (posicion == -1)
+                {
+                Operaciones.GetKey("Error: producto no encontrado");
+                }
+                else
+                {
+                precio[0, posicion] = Operaciones.GetFloat("Ingrese el nuevo precio: ", texto);
+                cantidad[0, posicion] = Operaciones.GetEntero("Ingrese la nueva cantidad: ", texto);
+                Operaciones.GetKey("Producto modificado exitosamente.");
+                }
             return 1;
-
         }
+        //Pantalla para Mostrar Inventario
         public static int MostrarInventario()
         {
             string texto = "===== Pantalla para Mostrar Inventario =====\n" +
@@ -172,36 +175,60 @@ namespace ProyectoFinal
             return 1;   
         }
 
-        //Menu-Gestionar Almacenes
+        //Pantalla para Agregar Almacén
         public static int AgregarAlmacen()
         {
-            almacen[0] = "Tiendita";
-            string texto = "===== Pantalla para Agregar Almacén =====\n"+
-                           "--------------------------------------------------\n"+
-                           "Ingrese el nombre del nuevo almacén: ";
+            string texto = "===== Pantalla para Agregar Almacén =====\n" +
+                           "--------------------------------------------------\n";
             Console.Write(texto);
-            almacen[alm_contador]= Console.ReadLine();
+            almacen[alm_contador] = Operaciones.GetString("Ingrese el nombre del nuevo almacén: ");
             Operaciones.GetKey("Almacén agregado exitosamente.");
             alm_contador++;
             return 2;
         }
+        //Pantalla para Eliminar Almacén
         public static int EliminarAlmacen()
         {
             string texto = "===== Pantalla para Eliminar Almacén =====\n" +
-                           "--------------------------------------------------\n" +
-                           "Ingrese el nombre del almacén a eliminar: ";
+                           "--------------------------------------------------\n";
             Console.Write(texto);
-            string nombre = Console.ReadLine();
-            bool eliminar = false;
+            string nombre = Operaciones.GetString("Ingrese el nombre del almacén a eliminar: ");
+            bool almacenEncontrado = false;
+
             for (int i = 0; i < alm_contador; i++)
             {
-                if (almacen[i] == nombre) eliminar = true;
-                if (eliminar) almacen[i] = almacen[i + 1];
+                if (almacen[i] == nombre)
+                {
+                    almacenEncontrado = true;
+                    for (int j = i; j < alm_contador - 1; j++)
+                    {
+                        almacen[j] = almacen[j + 1];
+
+                        for (int k = 0; k < pdto_contador[j + 1]; k++)
+                        {
+                            producto[j, k] = producto[j + 1, k];
+                            precio[j, k] = precio[j + 1, k];
+                            cantidad[j, k] = cantidad[j + 1, k];
+                        }
+                        pdto_contador[j] = pdto_contador[j + 1];
+                    }
+                    alm_contador--; 
+                    i--; 
+                }
             }
-            if (eliminar) alm_contador--;
-            Operaciones.GetKey("Almacén eliminado exitosamente.");
+            if (almacenEncontrado)
+            {
+                Operaciones.GetKey("Almacén eliminado exitosamente.");
+            }
+            else
+            {
+                Console.Write("--------------------------------------------------\n");
+                Console.Write("Almacén no encontrado.");
+                Console.ReadKey();
+            }
             return 2;
         }
+        //Pantalla para Moestrar Almacén
         public static int MostrarAlmacen()
         {
             string texto = "===== Pantalla para Mostrar Almacenes =====\n" +
@@ -214,38 +241,75 @@ namespace ProyectoFinal
             Console.Write(texto);
             Console.ReadKey();
             return 2;
-
         }
-        //Menu-Agregar y Extraer Productos
+        //Pantalla para Ingresar Producto en Almanecén
         public static int IngresarProductoAlmacen()
         {
-            string texto = Operaciones.SetTitulo("Pantalla para Ingresar Producto en Almacén");
+            string texto = "===== Pantalla para Ingresar Producto de Almacén =====\n" +
+               "--------------------------------------------------\n";
+            string texto1 = "";
+            if (alm_contador == 1)
+            {
+                Operaciones.GetKey("Sin almacenes"); return 3;
+            }
             for (int i = 1; i < alm_contador; i++)
             {
-                texto += i + ". " + almacen[i] + "\n";
+                texto1 += i + ". " + almacen[i] + "\n";
             }
-            Console.Write(texto);
-            int opcion1 = Operaciones.GetOpcion("Seleccione el almacén: ", texto, alm_contador);
-            string texto1 = "";
+            Console.Write(texto + texto1);
+            int opcion1 = Operaciones.GetOpcion("Seleccione el almacén: ", texto + texto1, alm_contador - 1); //opcion1 = i
+            string texto2 = "";
+            if (pdto_contador[0] == 0)
+            {
+                Operaciones.GetKey("Sin producto"); return 3;
+            }
             for (int i = 0; i < pdto_contador[0]; i++)
             {
-                texto1 += i + ". " + producto[0, i] + "\n";
+                texto2 += (i + 1) + ". " + producto[0, i] + "\n";
             }
-            Console.Write(texto1);
-            int opcion2 = Operaciones.GetOpcion("Seleccione el producto a ingresar: ", texto + texto1, pdto_contador[0]);
-            producto[opcion1, opcion2] = producto[0, opcion2];
-            int opcion3 = Operaciones.GetOpcion("Ingrese la cantidad a ingresar: ", texto + texto1, cantidad[0, opcion2]);
-            cantidad[opcion1, opcion2] += opcion3;
+            Console.Write(texto2);
+            int opcion2 = Operaciones.GetOpcion("Seleccione el producto a ingresar: ", texto + texto2, pdto_contador[0]); //opcion2 = j
+            opcion2--;
+            int pos = -1;
+            for (int i = 0; i < pdto_contador[opcion1]; i++)
+            {
+                if (producto[opcion1, i] == producto[0, opcion2])
+                {
+                    pos = i;
+                    break;
+                }
+            }
+            int opcion3 = Operaciones.GetEntero("Ingrese la cantidad a ingresar: ", texto);
+            if (opcion3 > cantidad[0, opcion2])
+            {
+                Operaciones.GetKey("Error: No hay suficiente cantidad en la tienda.");
+                return 3;
+            }
+            else
+            {
+                if (pos == -1)
+                {
+                    pos = pdto_contador[opcion1];
+                    pdto_contador[opcion1]++;
+                    producto[opcion1, pos] = producto[0, opcion2];
+                }
+            }
+            producto[opcion1, pos] = producto[0, opcion2];
+            cantidad[opcion1, pos] += opcion3;
             cantidad[0, opcion2] -= opcion3;
             Operaciones.GetKey("Producto ingresado en el almacén exitosamente.");
             return 3;
         }
-        //Extraer Producto
-        public static int ExtraerProducto()
+        //Pantalla para Extraer Producto de Almacén
+        public static int ExtraerProductoAlmacen()
         {
             string texto = "===== Pantalla para Extraer Producto de Almacén =====\n" +
                "--------------------------------------------------\n";
             Console.Write(texto);
+            if  (alm_contador == 1)
+            {
+                Operaciones.GetKey("Sin almacenes"); return 2;
+            }
 
             for (int i = 1; i < alm_contador; i++)
             {
@@ -254,26 +318,40 @@ namespace ProyectoFinal
             Console.Write(texto);
             int opcionAlmacen = Operaciones.GetOpcion("Seleccione el almacén: ", texto, alm_contador);
             string TextoProducto = "";
-            for (int i = 0; i < pdto_contador[opcionAlmacen]; i++)
+            if (pdto_contador[opcionAlmacen]== 0)
             {
-                TextoProducto += i + ". " + producto[opcionAlmacen, i] + "\n";
+                Operaciones.GetKey("Sin productos"); return 2;
+            }
+            for(int i = 1; i < alm_contador; i++)
+            {
+                TextoProducto += (i + 1) + ". " + producto[opcionAlmacen, i] + "\n";
             }
             Console.Write(TextoProducto);
-            int opcionProducto = Operaciones.GetOpcion("Seleccione el producto a extraer: ", texto + TextoProducto, pdto_contador[opcionAlmacen]);
-            int cantidadAExtraer = Operaciones.GetEntero("Ingrese la cantidad a extraer: ", texto + TextoProducto);
-            if (cantidad[opcionAlmacen, opcionProducto] >= cantidadAExtraer)
+            int opcionproducto = Operaciones.GetOpcion("Seleccione el producto a extraer: ", texto + TextoProducto, pdto_contador[opcionAlmacen]);
+            opcionproducto--;
+            int tiendapos = 1;
+            for (int i = 0; i < pdto_contador[0]; i++)
             {
-                cantidad[opcionAlmacen, opcionProducto] -= cantidadAExtraer;
-                Console.WriteLine("Confirmación: Producto extraído del almacén exitosamente.");
+                if ( producto[0,1]== producto[opcionAlmacen,opcionproducto])
+                {
+                    tiendapos = i; 
+                }
             }
+            int cantidadAExtraer = Operaciones.GetEntero("Ingrese la cantidad a extraer: ", texto + TextoProducto);
+            if (cantidad[opcionAlmacen, opcionproducto] >= cantidadAExtraer)
+            {
+                cantidad[opcionproducto,opcionAlmacen] -= cantidadAExtraer;
+                cantidad[0, tiendapos] += cantidadAExtraer;
+                Console.WriteLine("Producto extraído del almacén exitosamente.");
+            }    
             else
             {
                 Console.WriteLine("Error: No hay suficiente cantidad en el almacén.");
             }
             Console.ReadKey();
-            return 2; 
+            return 3; 
         }
-
+        //Pantalla para Ver Stock Actual
         public static int VerStockActual()
         {
             string texto = "===== Pantalla para Ver Stock Actual =====\n" +
@@ -292,11 +370,12 @@ namespace ProyectoFinal
                 }
             }
             Console.Write(texto);
+            if (contador == 1)
+            {
+                Console.WriteLine("Sin stock");
+            }
             Console.ReadKey();
-
-
             return 3;
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -17,48 +17,57 @@ namespace ProyectoFinal
                 switch (opcion)
                 {
                     //Menus
-                    case 0: opcion = Pantallas.MenuPrincipal();
+                    case 0: 
+                        opcion = Pantallas.MenuPrincipal();
                         break;
-                    case 1: opcion = Pantallas.MenuGestionarProductos();
+                    case 1: 
+                        opcion = Pantallas.MenuGestionarProductos();
                         break;
-                    case 2: opcion = Pantallas.MenuGestionarAlmacenes(); 
+                    case 2: 
+                        opcion = Pantallas.MenuGestionarAlmacenes(); 
                         break;
-                    case 3: opcion = Pantallas.MenuAgregarYExtraerProductos(); 
+                    case 3: 
+                        opcion = Pantallas.MenuAgregarYExtraerProductos(); 
                         break;
 
-                    //Menu-Gestionar Productos
+                    //Menú - Gestionar Productos
                     case 4:
                         opcion = Pantallas.AgregarProducto();
                         break;
                     case 5:
-                            opcion = Pantallas.EliminarProducto();
+                        opcion = Pantallas.EliminarProducto();
                         break;
-                    case 6: opcion = Pantallas.ModificarProducto();
+                    case 6: 
+                        opcion = Pantallas.ModificarProducto();
                         break;
-                    case 7: opcion = Pantallas.MostrarInventario();
-                        break;
-
-                    //Menu-Gestionar Almacenes
-                    case 8: opcion = Pantallas.AgregarAlmacen();
-                        break;
-                    case 9: opcion = Pantallas.EliminarAlmacen();
-                        break;
-                    case 10: opcion = Pantallas.MostrarAlmacen();
+                    case 7: 
+                        opcion = Pantallas.MostrarInventario();
                         break;
 
-                    //Menu-Agregar y Extraer Productos
-                    case 11: opcion = Pantallas.IngresarProductoAlmacen();
+                    //Menú - Gestionar Almacenes
+                    case 8: 
+                        opcion = Pantallas.AgregarAlmacen();
+                        break;
+                    case 9: 
+                        opcion = Pantallas.EliminarAlmacen();
+                        break;
+                    case 10: 
+                        opcion = Pantallas.MostrarAlmacen();
+                        break;
+
+                    //Menú - Agregar y Extraer Productos
+                    case 11: 
+                        opcion = Pantallas.IngresarProductoAlmacen();
                         break;
                     
                     case 12: 
-                        opcion = Pantallas.ExtraerProducto(); 
+                        opcion = Pantallas.ExtraerProductoAlmacen(); 
                         break;
                     case 13: 
                         opcion = Pantallas.VerStockActual(); 
                         break;
                 }
             }
-
         }
     }
 }
